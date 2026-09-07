658a3762e0205f8b4378a0b1efb77e73a16f494e
Sohum Sabhani <sohumsabhani@gmail.com>
2026-09-07 13:57:24 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (43.06%) - LeetSync

---
+++ 2480-find-subarrays-with-equal-sum/find-subarrays-with-equal-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean findSubarrays(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            int sum = nums[i] + nums[i + 1];
+            
+            if (seen.contains(sum)) {
+                return true;
+            }
+            
+            seen.add(sum);
+        }
+        
+        return false;
+    }
+}
